6681c6ee2ac8198b62e5030692549fc3810a5ed3
Fabian Lesniak <fabian@lesniak-it.de>
2017-08-23 14:13:54 +0200

    add WWW-Authenticate header, logging, auth cache

--- nginx-auth-service.py
+++ nginx-auth-service.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
-from flask import Flask, abort, request
+import logging
+from flask import Flask, abort, request, Response
 import mysql.connector
 import hashlib
+import sys
 
 app = Flask(__name__)
+
+#log_level = logging.WARNING
+log_level = logging.INFO
+#log_level = 0 # to enable full logging of werkzeug's logger
+auth_cache = []
 
 def generate_hash(user, password):
   m = hashlib.md5()
@@ -12,25 +19,46 @@ def generate_hash(user, password):
   return m.hexdigest()
 
 def check_auth(user, password):
+  global auth_cache
+  authorized = False
   auth_hash = generate_hash(user, password)
+  if auth_hash in auth_cache:
+    app.logger.info("Auth of {} from cache".format(user))
+    return True
   try:
     cnx = mysql.connector.connect(user='user', passwd='password', database='auth_database')
-    cursor = cnx.cursor()
-    cursor.execute('SELECT count(*) FROM user WHERE hash=%s', (str(auth_hash),))
   except Exception as e:
-    print("Database error: {}".format(e))
+    app.logger.error("Failed to connect to database: {}".format(e))
     return False
-  authorized = next(cursor)[0] == 1
-  cursor.close()
-  cnx.close()
+  try:
+    cursor = cnx.cursor()
+    cursor.execute('SELECT count(*) FROM users WHERE hash=%s', (str(auth_hash),))
+    authorized = next(cursor)[0] == 1
+    cursor.close()
+  except Exception as e:
+    app.logger.error("Database error: {}".format(e))
+  finally:
+    cnx.close()
+  if authorized:
+    auth_cache += [auth_hash]
   return authorized
+
+# https://stackoverflow.com/questions/7877230/standard-401-response-when-using-http-auth-in-flask
+@app.errorhandler(401)
+def custom_401(error):
+  return Response('Access denied', 401, {'WWW-Authenticate': 'Basic realm="our.web.site"'})
 
 @app.route('/auth', methods=['GET'])
 def login():
   auth = request.authorization
-  if check_auth(auth.username, auth.password):
+  if auth is not None and check_auth(auth.username, auth.password):
     return "Authentication successful"
-  abort(401)
+  else:
+    abort(401)
 
 if __name__ == '__main__':
-  app.run(port = 9000)
+  # logs http requests to flask
+  logging.getLogger('werkzeug').setLevel(log_level)
+  # logs everything else using app.logger
+  logging.getLogger(__name__).setLevel(log_level)
+  app.run(host='::', port=9000)
